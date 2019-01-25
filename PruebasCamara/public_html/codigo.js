a0dabbc75e695474fe23049f6bf71b65372c2c90
GabrielaCarrasco <gabrielacarrasco.94@gmail.com>
2019-01-25 12:43:01 -0300

    Prueba mensaje

--- PruebasCamara/public_html/codigo.js
+++ PruebasCamara/public_html/codigo.js
@@ -1,78 +1,69 @@
 $("#btnPrueba5").click(funcionBoton5);
 
-    var video = document.querySelector('#video'), canvas = document.querySelector('#cam'),
-    img=document.querySelector("#fotoDocumento");
-function funcionBoton5(){
-       $(":mobile-pagecontainer").pagecontainer("change", "#pbtn5");
-       iniciarCamara();
-      //window.addEventListener('load',iniciarCamara());
+var video = document.querySelector('#video'), canvas = document.querySelector('#cam'),
+        img = document.querySelector("#fotoDocumento");
+
+
+function funcionBoton5() {
+    $(":mobile-pagecontainer").pagecontainer("change", "#pbtn5");
+    iniciarCamara();
 }
-    $("#btnSacarFoto").click(sacarFoto);
-function iniciarCamara(){
-      var canvas = document.getElementById('canvas'),
-    context = canvas.getContext('2d'),
+
+function iniciarCamara() {
     
-    video= document.getElementById('video');
-    navigator.getMedia = navigator.getUserMedia || navigator.mediaDevices.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia; 
+    var canvas = document.getElementById('canvas'),
+            context = canvas.getContext('2d'),
+            video = document.getElementById('video');
     
+    navigator.getMedia = navigator.getUserMedia || 
+                                        navigator.mediaDevices.getUserMedia || 
+                                        navigator.webkitGetUserMedia ||
+                                        navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
     navigator.getMedia({
-        video:true,
-        audio:false
-    }, function (stream){
-        video.srcObject=stream;
+        video: true,
+        audio: false
+    }, function (stream) {
+        video.srcObject = stream;
         video.play();
-    }, function (error){
+    }, function (error) {
         alert(error.code);
+        alert(error.message);
     });
 }
- /*   var p = navigator.mediaDevices.getUserMedia({ audio: false, video: true });
-p.then(function(mediaStream) {
-  var video = document.querySelector('video');
-  video.src = window.URL.createObjectURL(mediaStream);
-  video.src=mediaStream;
-  video.onloadedmetadata = function(e) {
-alert ("aca");
-  };
-});
 
-p.catch(function(err) { console.log(err.name); }); // always check for errors at the end.
-    /*
-    navigator.getUserMedia = (navigator.getUserMedia || navigator.mediaDevices.getUserMedia() || navigator.webkitGetUserMedia);
-    if(navigator.getUserMedia){
-        navigator.getUserMedia({video:true}, function(stream){
-            //video.src= window.URL.createObjectURL(stream);
-            video.src=stream;
-            video.play();  
-        },function(e){alert(e);})
-    }else alert ("No anda ");   */
-    
-  //  video.addEventListener('loadedmetadata', function (){canvas.width= videoWidth; canvas.height=videoHeight;},false);
-    
+$("#btnSacarFoto").click(sacarFoto);
+function sacarFoto() {
+    var canvas = document.getElementById('canvas');
+    canvas.getContext('2d').drawImage(video, 0, 0);
+    var imgData = canvas.toDataURL('image/png');
+    img.setAttribute("src", imgData);
+}
 
-    
 
-    
-    
-//}
 
-    function sacarFoto(){
-        alert("ACA");
-          var canvas = document.getElementById('canvas');
-        canvas.getContext('2d').drawImage(video,0,0);
-        var imgData= canvas.toDataURL('image/png');
-        img.setAttribute("src",imgData);
-    }
 
-function OCR(){
-Tesseract.recognize(archivo)
-  .progress(function  (p) { console.log('progress', p)  })
-  .catch(err => console.error(err))
-  .then(function (result) {
-   obtenerNumeroCI(result.text);
-  });
-  };
-  
-function obtenerNumeroCI(texto){
-    $("#resPage1").empty(); 
+
+
+
+
+
+
+
+
+function OCR() {
+    Tesseract.recognize(archivo)
+            .progress(function (p) {
+                console.log('progress', p)
+            })
+            .catch(err => console.error(err))
+            .then(function (result) {
+                obtenerNumeroCI(result.text);
+            });
+}
+;
+
+function obtenerNumeroCI(texto) {
+    $("#resPage1").empty();
     $("#resPage1").append(texto);
 }
